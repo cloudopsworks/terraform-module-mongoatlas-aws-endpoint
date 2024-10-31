8f9f1f23ba6e8445c12f190b6f9e264f6fd5b29d
Cristian Beraha <cristian@cloudops.works>
2024-10-31 15:51:34 -0300

    chore Initial

--- main.tf
+++ main.tf
@@ -4,3 +4,55 @@
 #            Distributed Under Apache v2.0 License
 #
 
+locals {
+  sglist = try(var.vpc.security_group_ids, [])
+
+}
+
+data "mongodbatlas_project" "this" {
+  count = var.project_name != "" ? 1 : 0
+  name  = var.project_name
+}
+
+data "mongodbatlas_network_container" "this" {
+  container_id = var.atlas_container_id
+  project_id   = var.project_id != "" ? var.project_id : data.mongodbatlas_project.this[0].project_id
+}
+
+resource "mongodbatlas_privatelink_endpoint" "this" {
+  project_id    = var.project_id != "" ? var.project_id : data.mongodbatlas_project.this[0].project_id
+  provider_name = data.mongodbatlas_network_container.this.provider_name
+  region        = data.mongodbatlas_network_container.this.region_name
+}
+
+resource "aws_security_group" "default" {
+  vpc_id = var.vpc.vpc_id
+  name   = "mongodb-ep-${local.system_name}"
+
+  tags_all = merge({
+    Name = "mongodb-ep-${local.system_name}"
+  }, local.all_tags)
+}
+
+resource "aws_security_group_rule" "default_ingress" {
+  security_group_id = aws_security_group.default.id
+  type              = "ingress"
+  protocol          = "TCP"
+  from_port         = try(var.settings.port, 27017)
+  to_port           = try(var.settings.port, 27017)
+}
+
+resource "aws_vpc_endpoint" "this" {
+  vpc_id             = var.vpc.vpc_id
+  service_name       = mongodbatlas_privatelink_endpoint.this.endpoint_service_name
+  vpc_endpoint_type  = "Interface"
+  subnet_ids         = var.vpc.subnet_ids
+  security_group_ids = concat(local.sglist, [aws_security_group.default.id])
+}
+
+resource "mongodbatlas_privatelink_endpoint_service" "this" {
+  project_id          = data.mongodbatlas_project.this.project_id
+  private_link_id     = mongodbatlas_privatelink_endpoint.this.private_link_id
+  endpoint_service_id = aws_vpc_endpoint.this.id
+  provider_name       = "AWS"
+}
